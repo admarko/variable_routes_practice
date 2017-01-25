531a7eca22ca7267757b15e83449dea902117e3f
Alex Markowitz <admarko@gmail.com>
2017-01-25 17:38:29 -0600

    Basic Calc w/Ruby
    Take inputs through URLs to find square, square root, random numbers,
    and loan payment calc
    **Good use of params hash

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,36 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+    #Parameters: {"num"=>"345"}
+    x = params["num"].to_i
+    @val = x
+    @sval = x*x
+    render("square.html.erb")
+  end
+
+  def square_root
+    x = params["num"].to_i
+    @val = x
+    @sval = Math.sqrt(x)
+    render("square_root.html.erb")
+  end
+
+  def random
+    @min = params["min"].to_i
+    @max = params["max"].to_i
+    @rand = @min + rand(@max-@min)
+    render("random.html.erb")
+  end
+
+  def payment
+    @rate = (params["rate"].to_i)/100.0
+    @years = params["years"].to_i
+    @loan = params["loan"].to_i
+    months = @years*12
+    rate = @rate/1200.0
+    @monthly_payment = (@loan * (rate + (rate / ((1+rate) ** months - 1)))).round(2)
+    render("payment.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square/:num", {:controller => "calculations", :action => "square"})
+  get("/square_root/:num", {:controller => "calculations", :action => "square_root"})
+  get("/random/:min/:max", {:controller => "calculations", :action => "random"})
+  get("/payment/:rate/:years/:loan", {:controller => "calculations", :action => "payment"})
 end
